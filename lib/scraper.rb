589e9d2d291edf67f873adf25e7b277669f6eea6
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-22 01:51:21 +0000

    add scraping functionality to #scrape_missions_from_url

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,16 @@
 class Scraper
 
   def scrape_missions_from_url(url)
+    missions = []
+    Nokogiri::HTML(open(url)).css("div.list-text-content").each do |mission|
+      new_mission = {}
+      new_mission[:title] = mission.css(".content_title").text.strip
+      new_mission[:launch_date] = mission.css(".article_teaser_body").first.text
+      new_mission[:description] = mission.css(".article_teaser_body").last.text
+      missions << new_mission
+    end
+
+    missions
   end
 
 end
