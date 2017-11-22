1afab845c5ba5b7894fc8aa7774e37f3f9becd6d
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-22 01:15:17 +0000

    add basic nav functionality to #main_menu

--- lib/cli.rb
+++ lib/cli.rb
@@ -13,6 +13,24 @@ class CLI
       3. Future Missions
       Enter 'exit' to quit
     DOC
+    input = gets.chomp
+    case input
+    when '1'
+      #print list of current Missions
+      puts "You've selected Current Missions."
+    when '2'
+      #print list of past Missions
+      puts "You've selected Past Missions."
+    when '3'
+      #print list of future Missions
+      puts "You've selected Future Missions."
+    when 'exit'
+      puts "Goobye!"
+    else
+      puts "I'm sorry, that's not an option."
+      puts "Please choose from the available options."
+      self.main_menu
+    end
   end
 
 end
