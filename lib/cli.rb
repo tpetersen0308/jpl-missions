1b335698efbcd6d33695ddaf24eaf32ba9118eb1
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-22 19:46:07 +0000

    add loop back to main menu to #list_missions

--- lib/cli.rb
+++ lib/cli.rb
@@ -37,6 +37,15 @@ class CLI
       Mission.all.each.with_index do |mission, index|
         puts "#{index + 1}: #{mission.title}"
       end
+      puts "To learn more, select a mission."
+      puts "To return to the main menu, enter 'exit.'"
+      input = gets.chomp
+      if input.between?('1', Mission.all.size.to_s)
+        puts "More information about selected mission..."
+      elsif input == "exit"
+        main_menu
+      else puts "Please select from the available options"
+      end
     when '2'
       missions_array = Scraper.scrape_missions_from_url('https://www.jpl.nasa.gov/missions/?search=&type=past&missions_target=&mission_type=&launch_date=#submit')
       Mission.create_missions_from_array(missions_array)
